bebc312893ca4ae43ea8b3626b8ff19fce9c4b54
BlueIronS <prietosamuel6712@gmail.com>
2025-04-10 19:43:32 -0500

    Update and rename sketch_mar26a.ino to Codigo_oled.ino

--- Codigo_oled.ino
+++ Codigo_oled.ino
@@ -1,35 +1,4 @@
-// simple project using Arduino UNO and 128x64 SSD1306 IIC OLED Display to display animated icon from www.icons8.com
-
-// created by upir, 2023
-// youtube channel: https://www.youtube.com/upir_upir
-
-// YOUTUBE VIDEO: https://youtu.be/o3PhC_VJdXo
-
-// Links from the video:
-// WOKWI animator: https://animator.wokwi.com/
-// Animated icons: https://icons8.com/icons/set/popular--animated
-// Infinity icon: https://icons8.com/icons/set/infinity--animated
-// 128x64 SSD1306 OLED Display 1.54": https://s.click.aliexpress.com/e/_DCYdWXb 
-// 128x64 SSD1306 OLED Display 0.96": https://s.click.aliexpress.com/e/_DCKdvnh
-// 128x64 SSD1306 OLED Display 2.42": https://s.click.aliexpress.com/e/_DFdMoTh
-// Arduino UNO: https://s.click.aliexpress.com/e/_AXDw1h
-// Arduino breadboard prototyping shield: https://s.click.aliexpress.com/e/_ApbCwx
-// Image2cpp (convert image to c-style array): https://javl.github.io/image2cpp/
-// Photopea (online graphics editor like Photoshop): https://www.photopea.com/
-// Piskel Application (online animation editor): https://www.piskelapp.com/p/create/sprite
-
-// Related videos with Arduino UNO and 128x64 OLED screen:
-// Arduino OLED menu: https://youtu.be/HVHVkKt-ldc
-// U8g vs U8g2: https://youtu.be/K5e0lFRvZ2E
-// Arduino Parking Sensor - https://youtu.be/sEWw087KOj0
-// Turbo pressure gauge with Arduino and OLED display - https://youtu.be/JXmw1xOlBdk
-// Arduino Car Cluster with OLED Display - https://youtu.be/El5SJelwV_0
-// Knob over OLED Display - https://youtu.be/SmbcNx7tbX8
-// Arduino + OLED = 3D ? - https://youtu.be/kBAcaA7NAlA
-// Arduino OLED Gauge - https://youtu.be/xI6dXTA02UQ
-// Smaller & Faster Arduino - https://youtu.be/4GfPQoIRqW8
-// Save Image from OLED Display to PC - https://youtu.be/Ft2pRMVm44E
-
+//Creditos a https://www.youtube.com/watch?v=o3PhC_VJdXo&t=351s
 
 #include <Arduino.h>
 #include <U8g2lib.h>
